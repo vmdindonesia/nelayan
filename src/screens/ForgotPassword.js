1e173320390f1fadcf9866484a6bcdfaca249afb
Raosan <raosanf.lillahi@gmail.com>
2018-02-16 13:52:48 +0700

    styling forgot password

--- src/screens/ForgotPassword.js
+++ src/screens/ForgotPassword.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { connect } from 'react-redux'
 import { Container, ContainerSection, Input, Button, Spinner } from '../components/common'
+import { COLOR } from '../constants'
 
 class ForgotPassword extends Component {
 	static navigationOptions = {
-		title: 'Lupa Password',
-		headerRight: <View />
+		header: null
 	}
 
 	constructor(props) {
@@ -27,7 +27,7 @@ class ForgotPassword extends Component {
 		}
 
 		return (
-			<Button onPress={() => this.login()}>
+			<Button>
 				Kirim 
 			</Button>
 		)
@@ -37,11 +37,29 @@ class ForgotPassword extends Component {
 		const { email } = this.state
 
 		return (
-			<View style={{flex: 1}}>
+			<View style={{flex: 1, paddingTop: 100, backgroundColor: '#2b76d2'}}>
 				<Container>
+					<ContainerSection>
+						<Image
+							style={{width: 35, height: 40, marginLeft: 10}}
+							source={require('../../assets/ic_password_white.png')} 
+						/>
+					</ContainerSection>
+				</Container>
+				<Container>
+					<ContainerSection>
+						<Text style={{color: '#fff'}}>Kami akan mengirimkan konfirmasi & tautan penyetelan ulang kata sandi anda ke email.</Text>
+					</ContainerSection>
+				</Container>
+				
+				<Container>
+					<ContainerSection>
+						<Text style={{color: '#fff', marginBottom: -15}}>Masukkan email</Text>
+					</ContainerSection>
 					<ContainerSection>
 						<Input
 							label='Email'
+							placeholder='Alamat email Anda'
 							onChangeText={val => this.onChange('email', val)}
 							value={email}
 						/>
