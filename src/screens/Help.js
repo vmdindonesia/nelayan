6e2169e9f4bc1f692b3c397e058c60f740a71edd
Raosan <raosanf.lillahi@gmail.com>
2018-03-02 11:13:41 +0700

    telpon dan email linking

--- src/screens/Help.js
+++ src/screens/Help.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text, Image } from 'react-native'
-import { Container, ContainerSection, Input, Button, Spinner } from '../components/common'
+import { View, Text, Image, Linking, TouchableOpacity } from 'react-native'
+import { Container, ContainerSection } from '../components/common'
 
 class Help extends Component {
 	static navigationOptions = {
@@ -23,9 +23,23 @@ class Help extends Component {
 					<View style={{justifyContent: 'center', alignItems: 'center'}}>
 						<Text>Hubungi</Text>
 						<Text>Pusat Bantuan</Text>
-						<Text style={{fontSize: 20, marginTop: 10, marginBottom: 10}}>+62 21 777 8888</Text>
+						<TouchableOpacity
+							onPress={() => Linking.openURL('tel:0217778888').catch(err => console.error('An error occurred', err))}
+						>
+							<View>
+								<Text style={{fontSize: 20, marginTop: 10, marginBottom: 10}}>
+									+62 21 777 8888
+								</Text>
+							</View>
+						</TouchableOpacity>
 						<Text>atau</Text>
-						<Text style={{fontSize: 20, marginTop: 10, marginBottom: 10}}>info@aruna.id</Text>
+						<TouchableOpacity
+							onPress={() => Linking.openURL('mailto:info@aruna.id').catch(err => console.error('An error occurred', err))}
+						>
+							<View>
+								<Text style={{fontSize: 20, marginTop: 10, marginBottom: 10}}>info@aruna.id</Text>
+							</View>
+						</TouchableOpacity>
 					</View>
 				</Container>
 			</View>
