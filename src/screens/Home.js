d9693ab7de47ac2113b29e179c4c8b2b7904ba9d
raosan <raosanf.lillahi@gmail.com>
2018-04-23 13:52:00 +0700

    bug onesignal addEventListener registered

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -30,7 +30,7 @@ class Home extends Component {
 	componentWillMount() {
 		OneSignal.addEventListener('received', this.onReceived)
 		OneSignal.addEventListener('opened', this.onOpened)
-		OneSignal.addEventListener('registered', this.onRegistered)
+		// OneSignal.addEventListener('registered', this.onRegistered)
 		OneSignal.addEventListener('ids', this.onIds)
 
 		this.props.unreadNotifFetch(this.props.user.token)
@@ -44,7 +44,7 @@ class Home extends Component {
 	componentWillUnmount() {
 		OneSignal.removeEventListener('received', this.onReceived)
 		OneSignal.removeEventListener('opened', this.onOpened)
-		OneSignal.removeEventListener('registered', this.onRegistered)
+		// OneSignal.removeEventListener('registered', this.onRegistered)
 		OneSignal.removeEventListener('ids', this.onIds)
 	}
 
@@ -70,9 +70,9 @@ class Home extends Component {
 		console.log('openResult: ', openResult)
 	}
 
-	onRegistered(notifData) {
-		console.log('Device had been registered for push notifications!', notifData)
-	}
+	// onRegistered(notifData) {
+	// 	console.log('Device had been registered for push notifications!', notifData)
+	// }
 
 	onIds(device) {
 		console.log('Device info: ', device)
